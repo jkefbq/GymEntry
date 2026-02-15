66af6b124e0f87c09dff01937601bec578ec3451
jkefbq <jkefbq@gmail.com>
2026-02-15 16:25:58 +1000

    init commit

---
+++ src/main/java/com/jkefbq/gymentry/GymEntryApplication.java
@@ -0,0 +1,13 @@
+package com.jkefbq.gymentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymEntryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymEntryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkefbq/gymentry/GymEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkefbq.gymentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymEntryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
